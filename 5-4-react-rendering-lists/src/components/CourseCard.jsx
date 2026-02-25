7d5a838d2c48a498c9d3b46cc695b41c9ed07ee1
OsamaKfupm <s202242980@kfupm.edu.sa>
2026-02-25 10:23:37 +0300

    1st submission

--- 5-4-react-rendering-lists/src/components/CourseCard.jsx
+++ 5-4-react-rendering-lists/src/components/CourseCard.jsx
@@ -11,15 +11,19 @@ export default function CourseCard({ course, index, onMutateCourse }) {
 
   function toggleTask(id) {
     // TODO (TASK 4): toggle task.isDone for the task with matching id
+      onMutateCourse(index, (tasks) =>
+      tasks.map((t) => (t.id === id ? { ...t, isDone: !t.isDone } : t))
+    );
   }
 
   function deleteTask(id) {
     // TODO (TASK 4): remove the task with matching id
+    onMutateCourse(index, (tasks) => tasks.filter((t) => t.id !== id));
   }
 
   // Helpful hints for TASK 3 (optional to use)
-  // const hasTasks = course.tasks.length > 0;
-  // const allDone = hasTasks && course.tasks.every(t => t.isDone);
+  const hasTasks = course.tasks.length > 0;
+  const allDone = hasTasks && course.tasks.every(t => t.isDone);
 
   return (
     <article className="course card">
@@ -30,12 +34,14 @@ export default function CourseCard({ course, index, onMutateCourse }) {
             - course has tasks AND
             - all tasks are done
             Use logical && */}
+      {allDone && <span className="badge">All caught up</span>}
+
       </header>
 
       <section className="tasksSection">
 
         {/* DISPLAY ONLY: Show a message when there are no tasks */}
-        
+        {!hasTasks && <p className="empty">No tasks yet.</p>}
         <ul className="tasks">
           {/* TODO (TASK 2): Render tasks using course.tasks.map(...)
               For each task, render <TaskItem /> and pass:
@@ -44,6 +50,15 @@ export default function CourseCard({ course, index, onMutateCourse }) {
                 - onToggle={toggleTask}
                 - onDelete={deleteTask}
           */}
+
+          {course.tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              onToggle={toggleTask}
+              onDelete={deleteTask}
+            />
+          ))}
         </ul>
       </section>
     </article>
